52fb1a69a064bbfd729032c8330c4a96681be456
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-06-10 22:14:56 -0400

    validate params

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-function find(matrix) {
+function isValidMatrix(matrix) {
+  return Array.isArray(matrix);
+}
+
+function err(message) {
+  throw new Error(message);
+}
+
+function find(matrix, comparator) {
+  if (!matrix) {
+    err('matrix is required');
+  } else if (!isValidMatrix(matrix)) {
+    err('invalid matrix');
+  }
+
+  if (!comparator) {
+    err('comparator is required');
+  } else if (typeof comparator !== 'function') {
+    err('invalid comparator');
+  }
+
   return [];
 }
 
--- test/index.js
+++ test/index.js
@@ -2,10 +2,35 @@ import { expect } from 'chai';
 import app from '../src';
 
 describe('find', () => {
-  it('should return nothing', () => {
-    const matrix = [0, 0, 0, 0];
-    const expected = [];
-    const result = app.find(matrix);
-    expect(result).to.eql(expected);
+  describe('bad params', () => {
+    it('should throw when missing matrix', () => {
+      const fn = () => { app.find() };
+      expect(fn).to.throw('matrix is required');
+    });
+
+    it('should throw for invalid matrix', () => {
+      const fn = () => { app.find(1) };
+      expect(fn).to.throw('invalid matrix');
+    });
+
+    it('should throw when missing comparator', () => {
+      const fn = () => { app.find([]) };
+      expect(fn).to.throw('comparator is required');
+    });
+
+    it('should throw for invalid comparator', () => {
+      const fn = () => { app.find([], true) };
+      expect(fn).to.throw('invalid comparator');
+    });
+  });
+
+  describe('blobs', () => {
+    it('should return nothing', () => {
+      const matrix = [0, 0, 0, 0];
+      const comparator = () => {};
+      const expected = [];
+      const result = app.find(matrix, comparator);
+      expect(result).to.eql(expected);
+    });
   });
 });
