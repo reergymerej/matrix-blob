0e34730ffecd3f3b07338ba4611567b31ab9e229
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-06-11 00:03:23 -0400

    add method to get field

--- src/Matrix.js
+++ src/Matrix.js
@@ -48,20 +48,21 @@ Matrix.prototype.exists = function (row, column) {
     && this.rows[row].hasOwnProperty(column);
 };
 
-Matrix.prototype.east = function (row, column) {
-  return this.exists(row, column + 1)
+Matrix.prototype.field = function (row, column) {
+  const field = this.exists(row, column);
+  return field
     ? {
-      value: this.value(row, column + 1),
+      value: this.value(row, column),
     }
     : undefined;
 };
 
+Matrix.prototype.east = function (row, column) {
+  return this.field(row, column + 1);
+};
+
 Matrix.prototype.south = function (row, column) {
-  return this.exists(row + 1, column)
-    ? {
-      value: this.value(row + 1, column),
-    }
-    : undefined;
+  return this.field(row + 1, column);
 };
 
 Matrix.prototype.point = function (row, column) {
